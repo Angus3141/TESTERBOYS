e801505c4bf633127b97e1edf031a374774f1e99
Angus3141 <angus3141@gmail.com>
2025-05-26 13:44:15 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'inventory-app-v1';
+const URLs_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', evt => {
+  evt.waitUntil(
+    caches.open(CACHE_NAME)
+          .then(cache => cache.addAll(URLs_TO_CACHE))
+  );
+});
+
+self.addEventListener('fetch', evt => {
+  evt.respondWith(
+    caches.match(evt.request)
+          .then(resp => resp || fetch(evt.request))
+  );
+});
