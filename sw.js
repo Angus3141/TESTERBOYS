0019a155e3979949076b2b8915318066c94df55c
Angus3141 <angus3141@gmail.com>
2025-08-23 18:51:11 +1000

    Use absolute paths for navigation and assets

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'inventory-app-v1';
 const URLs_TO_CACHE = [
-  './',
-  './index.html',
-  './AddProducts.html',
-  './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png'
+  '/',
+  '/index.html',
+  '/AddProducts.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
 ];
 
 self.addEventListener('install', evt => {
